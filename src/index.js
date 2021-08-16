f50d0ff47159f909f5aa8de98b1bd331d2b86435
Роман Лодяты Александрович <68326671+roma111ka@users.noreply.github.com>
2021-08-16 15:58:39 +0300

    feat: brackets on

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(itemJoin => itemJoin.join(''));
+  let checkString =str;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (checkString.includes(bracketsConfig[i])) {
+      checkString = checkString.replace(bracketsConfig[i], '')
+      i = -1;
+    }
+  }
+  
+  return checkString.length === 0 ? true : false;
 }
